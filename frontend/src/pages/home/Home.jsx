e43cc2453bd8b5dd2deaa35ed9959a0aab53a0f5
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-05-21 11:47:08 +0200

    added 'add new recipe' function

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,18 +5,18 @@ import "./Home.css";
 
 export default function Home() {
     const [recipes, setRecipes] = useState(null);
+    const [newRecipeName, setNewRecipeName] = useState('');
 
-    useEffect(() => {
-        axios.get(API_URL + 'recipes')
-            .then(response => setRecipes(response.data))
-            .catch(error => {
-                console.error(error);
-                setRecipes([]);
-            });
-    }, []);
+    const API_RECIPES_URL = API_URL + 'recipes';
+
+    useEffect(loadRecipes, []);
 
     return <div className="homepage-container">
         <h1 className="homepage-title">Recipes</h1>
+        <div className="homepage-menu">
+            <input value={newRecipeName} onChange={event => setNewRecipeName(event.target.value)} />
+            <button disabled={newRecipeName.length === 0} onClick={submitNewRecipe}>Add Recipe</button>
+        </div>
         <div>
             {
                 recipes === null
@@ -27,4 +27,20 @@ export default function Home() {
             }
         </div>
     </div>
+
+    function submitNewRecipe() {
+        axios.post(API_RECIPES_URL, { title: newRecipeName })
+            .then(_ => loadRecipes())
+            .catch(console.error)
+            .finally(() => setNewRecipeName(''));
+    }
+
+    function loadRecipes() {
+        axios.get(API_RECIPES_URL)
+            .then(response => setRecipes(response.data))
+            .catch(error => {
+                console.error(error);
+                setRecipes([]);
+            });
+    }
 }
