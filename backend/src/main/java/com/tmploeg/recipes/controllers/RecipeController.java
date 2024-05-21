4e9b586628b83dc159dd0cb55e59ca774c7afb5a
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-05-21 10:54:11 +0200

    added getAll/getById endpoints

--- backend/src/main/java/com/tmploeg/recipes/controllers/RecipeController.java
+++ backend/src/main/java/com/tmploeg/recipes/controllers/RecipeController.java
@@ -1,8 +1,12 @@
 package com.tmploeg.recipes.controllers;
 
+import com.tmploeg.recipes.models.Recipe;
+import com.tmploeg.recipes.repositories.RecipeRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("api/v1/recipes")
 public class RecipeController {
-    @GetMapping
-    public ResponseEntity<String> hello(){
-        return ResponseEntity.ok("hello recipes!");
-    }
-}
+  private final RecipeRepository recipeRepository;
 
+  @GetMapping
+  public List<Recipe> getAll() {
+    return recipeRepository.findAll();
+  }
+
+  @GetMapping("{id}")
+  public ResponseEntity<Recipe> getById(@PathVariable Long id) {
+    return recipeRepository
+        .findById(id)
+        .map(ResponseEntity::ok)
+        .orElseGet(() -> ResponseEntity.notFound().build());
+  }
+}
