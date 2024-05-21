ca2b90d9061c4917fd4cf594c34d16fd87ced0c4
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-05-21 18:53:27 +0200

    added toolbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import AddRecipe from './pages/add-recipe/AddRecipe';
 import Home from './pages/home';
@@ -6,13 +6,21 @@ import Ingredients from './pages/ingredients';
 import Recipe from './pages/recipe';
 
 function App() {
+  const navigate = useNavigate();
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/ingredients" element={<Ingredients />} />
-      <Route path="/recipe/:id" element={<Recipe />} />
-      <Route path="/add-recipe" element={<AddRecipe />} />
-    </Routes>
+    <div>
+      <div className="toolbar">
+        <div onClick={() => navigate('/')}>Home</div>
+        <div onClick={() => navigate('/ingredients')}>Ingredients</div>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ingredients" element={<Ingredients />} />
+        <Route path="/recipe/:id" element={<Recipe />} />
+        <Route path="/add-recipe" element={<AddRecipe />} />
+      </Routes>
+    </div>
   )
 }
 
